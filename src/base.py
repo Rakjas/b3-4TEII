5cf783e43d01b4e9f92d188dee7da8f6e0e4439d
Luis Sánchez Casanueva <luis.sanchezc@um.es>
2020-05-18 17:10:04 +0200

    segundoMétodoNativo

--- src/base.py
+++ src/base.py
@@ -43,6 +43,7 @@ def plot_values(values_in, values_out, line_else_bars=True, width=0.5):
     plt.xlabel('Array indices')
     plt.ylabel('Values')
 
+
 # Código main:
 def main():   
     # Control de argumentos de línea de comandos:
@@ -53,12 +54,12 @@ def main():
     #reading fEntrada
     try:
         f = open(sys.argv[1])
-        list = f.readlines()
+        lista = f.readlines()
     except:
         print("No se encuentra el archivo de Entrada")
         sys.exit(-1)
         
-    slist = sorted(list)
+    slist = sorted(lista)
     
     #parsing fEntrada
     try:
@@ -71,15 +72,23 @@ def main():
         sys.exit(-1)
         
         
-        
+   
     #Creamos las soluciones
     
-    #Metodo 1
+    #Método 1
     t0_sol1 = time.time_ns()
-    sol1 = set(slist)
+    sol1 = sorted(set(slist))
     texec_sol1 = (time.time_ns()-t0_sol1)/1.0e9
     
     print("La opcion 1: set ha tardado {} segundos en ejecutarse.".format(texec_sol1))
+    
+    #Método 2
+    t0_sol2 = time.time_ns()
+    sol2 = list(set(slist))
+    texec_sol2 = (time.time_ns()-t0_sol2)/1.0e9
+    
+    
+    print("La opcion 2: set ha tardado {} segundos en ejecutarse.".format(texec_sol2))
     
     #Guardamos la solucion
     
@@ -97,6 +106,8 @@ def main():
        
     
     # Mostramos gráficas (de líneas y de barras) y continuamos:        
+    print("Comenzamos el plot de las comparativas")
+    
     plot_values(slist, sol1)
     show_plot_and_wait_for_key()
     plot_values(slist, sol1, line_else_bars=False)
